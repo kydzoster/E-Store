61b89bdf0c7506f53dd728e32d225a72f1f8e371
Martins Silins <kydzoster@gmail.com>
2020-06-12 20:08:32 +0000

    updated sorting and product count to product page

--- products/views.py
+++ products/views.py
@@ -28,6 +28,9 @@ def all_products(request):
                 # we annotate current list of products with a new field
                 products = products.annotate(lower_name=Lower('name'))
 
+            if sortkey == 'category':
+                sortkey = 'category__name'
+
             # we check if there is a direction
             if 'direction' in request.GET:
                 direction = request.GET['direction']
