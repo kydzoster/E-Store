233f52ea3682aeb31973d8085841a73347ec03a1
Martins Silins <kydzoster@gmail.com>
2020-06-20 12:55:47 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Gw5zKFMQPCXg8bq9sqH92oj7ibm09aYAWZt6d9mcrJjJLQuSXnomAWabWr78UZaJrL7jLJbeFPV99DyRRGxeJW000baUufSl0',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
